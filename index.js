cba9c77abdf38a090b332c0bffc945a9fb7b7d73
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-18 19:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.chartAt(0).toUpperCase()+str.substring(1);
+  }
+  
+   static sanitize(str){
+     return str.replace(/^-,['a-zA-Z\s]+/g," ");
+   }
+   static titleize(str){
+     return 
+   }
 }
